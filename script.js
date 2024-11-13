80301fd9a71d10ef849439dd5a8c6de48aec7967
TalitaGabriela <talitagabriela1216@gmail.com>
2024-11-12 21:38:46 -0400

    Correção: persistência do contador com localStorage

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let counter = 0;
+let counter = localStorage.getItem('counter') || 0;
+document.getElementById('counter').textContent = counter;
+
 document.getElementById('increment').addEventListener('click', function() {
   counter++;
   document.getElementById('counter').textContent = counter;
+  localStorage.setItem('counter', counter);
 });
